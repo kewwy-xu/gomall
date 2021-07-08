fc61b9714c5fa2972db6ecce1c0f1cfa2df913b9
徐汝明 <2229807049@qq.com>
2021-07-08 20:07:31 +0800

    ':3'

--- src/main/java/com/stack/dogcat/gomall/message/entity/ChatMessage.java
+++ src/main/java/com/stack/dogcat/gomall/message/entity/ChatMessage.java
@@ -1,9 +1,6 @@
 package com.stack.dogcat.gomall.message.entity;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableLogic;
-import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
